682f1b937a5dc78d3e3c4da26bfdaa41769153d7
Felipe Apostol <flipjs.io@gmail.com>
2015-01-09 02:05:10 +0000

    update navbar

--- public/modules/lens/config/lens.client.config.js
+++ public/modules/lens/config/lens.client.config.js
@@ -4,8 +4,8 @@
 angular.module('lens').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Lens', 'lens', 'dropdown', '/lens(/create)?');
-		Menus.addSubMenuItem('topbar', 'lens', 'List Lens', 'lens');
-		Menus.addSubMenuItem('topbar', 'lens', 'New Len', 'lens/create');
+		Menus.addMenuItem('topbar', 'Lenses', 'lens', 'dropdown', '/lens(/create)?');
+		Menus.addSubMenuItem('topbar', 'lens', 'List Lenses', 'lens');
+		Menus.addSubMenuItem('topbar', 'lens', 'New Lens', 'lens/create');
 	}
 ]);
